910ee6d97078b90f80235279e535aa4ab50bcc87
James Williams <bonfirejunkie@gmail.com>
2017-10-03 15:09:46 -0700

    Setup start scrip and heroki

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs'); 
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app= express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -10,7 +11,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) =>{
     var now = new Date().toString();
-    var log = '${now}: ${req.method} ${res.url}';
+    var log = `${now}: ${req.method} ${res.url}`;
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
@@ -63,6 +64,6 @@ app.get('/bad', (req, res) =>{
 });
 
 //Route
- app.listen(3000, ()=> {
+ app.listen(port, ()=> {
      console.log('Server is up on 3000');
  });  //common port for dev locally

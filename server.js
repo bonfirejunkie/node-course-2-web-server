e35d610b5722cb2618858cbc881ad839488cdf2b
James Williams <bonfirejunkie@gmail.com>
2017-10-03 13:38:10 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs'); 
+const fs = require('fs');
+
+var app= express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+//uses middleware
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = '${now}: ${req.method} ${res.url}';
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next(); 
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+});
+ //http handeler,  req has all the header information for the request
+ //app.get('/', (req, res) => {
+ //   res.send({
+ //       name: 'Andrew',
+ //       likes: [
+ //           'Biking', 
+ //           'Cities'
+ //       ]
+ //});
+ //});
+
+//Home Route
+app.get('/', (req, res) =>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the homepage!',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+ //Route
+ app.get('/about', (req, res) =>{
+     res.render('about.hbs', {
+         pageTitle: 'About page',
+         currentYear: new Date().getFullYear()
+     }); 
+ });
+
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+//Route
+ app.listen(3000, ()=> {
+     console.log('Server is up on 3000');
+ });  //common port for dev locally
